103eb538950ee284cba7cfc36a18436808c8501c
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-07-13 16:41:44 -0700

    Fixing renderbugs

--- models/connection.js
+++ models/connection.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose") // MongoDB ORM
 
 const DATABASE_URL = process.env.DATABASE_URL // get DATABASE URI
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true }, { useUnifiedTopology: true}) // connect to MongoDB
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection
     .on("open", () => {console.log("Connected")})
     .on("close", () => {console.log("Disonnected")})
-    .on("error", () => {console.log("error")})
+    .on("error", (error) => { console.log(error) });
 
 module.exports = mongoose
