9d399ec1b33caf4367a4bb4a8664600206f46966
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-07-13 16:45:15 -0700

    Fixing renderbugs

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config() // bring in .env variables
 const mongoose = require("mongoose") // MongoDB ORM
 
-const DATABASE_URL = process.env.DATABASE_URL // get DATABASE URI
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
